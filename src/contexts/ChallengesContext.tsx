ed39c7189573ae72adb53c34b0ff5292971d1742
NicholasNeto <nicholasneto@outlook.com>
2021-02-25 23:05:09 -0300

    created context for new challenge

--- src/contexts/ChallengesContext.tsx
+++ src/contexts/ChallengesContext.tsx
@@ -1,22 +1,47 @@
 import { createContext, useState, ReactNode } from 'react'
 
-export const ChallengesContext = createContext({})
+
+
+
+interface ChallengesContextData {
+    level: number,
+    currentExperience: number,
+    challengesCompleted: number,
+    levelUp: ()=> void,
+    startNewChallenges: ()=> void,
+}
 
 interface ChallengesProviderProps {
     children: ReactNode
 }
 
 
+export const ChallengesContext = createContext({} as ChallengesContextData)
+
 export function ChallengesProvider({ children }: ChallengesProviderProps) {
     const [level, setLevel] = useState(1)
+    const [currentExperience, setCurrentExperience] = useState(0)
+    const [challengesCompleted, setChalengesCompleted] = useState(0)
 
 
     function levelUp() {
         setLevel(level + 1)
     }
 
+    function startNewChallenges() {
+        console.log('New Challenge')
+    }
+
     return (
-        <ChallengesContext.Provider value={{ level, levelUp }}>
+        <ChallengesContext.Provider
+            value={{
+                level,
+                currentExperience,
+                challengesCompleted,
+                levelUp,
+                startNewChallenges
+            }}
+        >
             {children}
         </ChallengesContext.Provider>
     )
