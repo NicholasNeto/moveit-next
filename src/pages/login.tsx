6d241e8f350b6013ecb8364fe9df0b2fb90bd62f
NicholasNeto <nicholasneto@outlook.com>
2021-03-03 23:26:50 -0300

    teste with page login

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,13 +1,17 @@
 
-import styles from '../styles/pages/login.module.css';
+import styles from '../styles/pages/Login.module.css';
 
 export function Login() {
 
     return (
         <div className={styles.loginContainer}>
-            <img src="logo-full.svg" alt="logo" />
+            <img src="logo-full-white.svg" alt="logo" />
             <h1>Bem vindo</h1>
             <p>Faça login com seu Github para começar</p>
+            <div>
+                <input   name="phone" placeholder="Digite seu username"/>
+                <button><img src="icons/teste.svg" alt="logo" /></button>
+            </div>
         </div>
     )
 }
