853a7494358f617ad817549087c2f1e885bad696
NicholasNeto <nicholasneto@outlook.com>
2021-04-03 17:14:27 -0300

    created component SignOutButton

---
+++ src/components/SignOutButton/SignOutButton.tsx
@@ -0,0 +1,19 @@
+import { useState } from "react";
+
+import { useRouter } from 'next/router'
+import { signOut } from 'next-auth/client'
+
+import styles from './SignOutButton.module.css'
+
+export function SignOutButton() {
+
+    return (
+        <button
+            className={styles.signOutContainer}
+            type="button"
+            onClick={() => signOut()}
+        >
+            <img src="/icons/sign-out.svg" alt="House" />
+        </button>
+    )
+}
