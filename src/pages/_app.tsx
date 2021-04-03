52a9c3908345f0287b1a18507ef8aa43b9b874a8
NicholasNeto <nicholasneto@outlook.com>
2021-04-03 17:14:59 -0300

    add Menu

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,18 @@
+import { Provider as NextAuthProviderGitHub } from 'next-auth/client'
+import { Menu } from '../components/menu/Menu';
 import '../styles/global.css';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+
+    <NextAuthProviderGitHub session={pageProps.session}>
+      <div style={{display: 'flex'}}>
+        <Menu />
+        <Component {...pageProps} />
+      </div>
+    </NextAuthProviderGitHub>
+  )
+
 }
 
 export default MyApp
