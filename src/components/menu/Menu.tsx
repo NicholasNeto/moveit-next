8d268f465477228f8fafd317518c1fbfc67d0439
NicholasNeto <nicholasneto@outlook.com>
2021-04-04 21:10:27 -0300

    component menu

--- src/components/menu/Menu.tsx
+++ src/components/menu/Menu.tsx
@@ -3,11 +3,15 @@ import { SignOutButton } from '../SignOutButton/SignOutButton';
 import styles from './Menu.module.css';
 
 
-export function Menu(){
-    return(
+export function Menu() {
+    return (
         <div className={styles.menuContainer}>
-            <img src="/icons/logo.svg" alt="logo" />
-            <img src="/icons/house.svg" alt="House" />
+            <div className={styles.logo}>
+                <img src="/icons/logo.svg" alt="logo" />
+            </div>
+            <div className={styles.home}>
+                <img src="/icons/house.svg" alt="House" />
+            </div>
             <SignOutButton />
         </div>
     )
