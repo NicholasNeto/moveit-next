b936d4684686a9d3c7f6c25d6f4d69612f8a62f8
NicholasNeto <nicholasneto@outlook.com>
2021-04-10 20:40:02 -0300

    add icon for plus

--- src/components/menu/Menu.tsx
+++ src/components/menu/Menu.tsx
@@ -2,16 +2,21 @@
 import { SignOutButton } from '../SignOutButton/SignOutButton';
 import styles from './Menu.module.css';
 import { ToggleSwitch } from '../ToggleSwitch/ToggleSwitch';
+import { FaPlus } from 'react-icons/fa';
 
 
 export function Menu() {
     return (
         <div className={styles.menuContainer}>
             <div className={styles.logo}>
-                <img src="/icons/logo.svg" alt="logo" />
+                <img src="/icons/logo.svg" style={{ width: '48px', height: '42px' }} alt="logo" />
             </div>
             <div className={styles.home}>
                 <img src="/icons/house.svg" alt="House" />
+            </div>
+
+            <div className={styles.plus}>
+                <FaPlus />
             </div>
             <div>
                 <ToggleSwitch />
