3f17482ca1d374df8c660f588faab1d24d4bf6bd
NicholasNeto <nicholasneto@outlook.com>
2021-04-11 15:03:34 -0300

    created page modal for test

---
+++ src/pages/modal.tsx
@@ -0,0 +1,11 @@
+import { PlusChallenges } from "../components/PlusChallenges/PlusChallenges";
+
+export default function Modal() {
+
+    return (
+        <div>
+            <button>show modal</button>
+            <PlusChallenges />
+        </div>
+    )
+}
