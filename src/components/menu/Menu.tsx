7aac927dafed27f5740ae2491a4332655e6f913d
NicholasNeto <nicholasneto@outlook.com>
2021-04-12 19:41:46 -0300

    onclik in icon Plus

--- src/components/menu/Menu.tsx
+++ src/components/menu/Menu.tsx
@@ -3,9 +3,15 @@ import { SignOutButton } from '../SignOutButton/SignOutButton';
 import styles from './Menu.module.css';
 import { ToggleSwitch } from '../ToggleSwitch/ToggleSwitch';
 import { FaPlus } from 'react-icons/fa';
+import { useContext } from 'react';
+import { PlusChallegeContext } from '../../contexts/PlusChallegeContext';
 
 
 export function Menu() {
+
+
+    const { handleAddChallege } =useContext(PlusChallegeContext)
+
     return (
         <div className={styles.menuContainer}>
             <div className={styles.logo}>
@@ -15,8 +21,8 @@ export function Menu() {
                 <img src="/icons/house.svg" alt="House" />
             </div>
 
-            <div className={styles.plus}>
-                <FaPlus />
+            <div className={styles.plus} >
+                <FaPlus onClick={() => handleAddChallege(true)} />
             </div>
             <div>
                 <ToggleSwitch />
