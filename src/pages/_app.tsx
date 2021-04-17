d599e245622b215978997acb34edfb9a8bb6de15
NicholasNeto <nicholasneto@outlook.com>
2021-04-16 23:20:37 -0300

    used ToastContainer

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,25 @@
 import { Provider as NextAuthProviderGitHub } from 'next-auth/client'
+import { ToastContainer } from 'react-toastify';
+
 import '../styles/global.css';
 
 function MyApp({ Component, pageProps }) {
   return (
 
     <NextAuthProviderGitHub session={pageProps.session}>
-      <Component {...pageProps} />
+      <Component {...pageProps} >
+        <ToastContainer
+          position="top-right"
+          autoClose={5000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+        />
+      </Component>
     </NextAuthProviderGitHub>
   )
 
